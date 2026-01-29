93dbe838b821c94ce722b5a836cf8be6af280d6c
kiranpasupuleti-eng <kiranpasupuleti47@gmail.com>
2026-01-29 13:02:02 +0530

    Armstrong.py is file added

---
+++ Armstrong.py
@@ -0,0 +1,38 @@
+'''Write a code find the give number is polindrome or not'''
+
+while True:
+    def is_Armstrong(num):
+        digits = len(str(num))
+        temp = num
+        total = 0
+
+        while temp > 0:
+            digit = temp % 10
+            total += digit ** digits
+            temp //= 10
+
+        return total == num
+    '''
+    num = int(input("Enter a number: "))
+    print(num,"is An Armstrong Number" if is_Armstrong(num) else "Not An Armstrong")
+    '''
+    
+    #Range Some Number To Some Number
+    def get_Armstrong(start,end):
+        res=""
+        for i in range(st,end+1):
+            if is_Armstrong(i):
+                res=res+str(i)+"|"
+        return res
+
+    st=int(input("Enter Starting Number:"))
+    ed=int(input("Enter Ending Number:"))
+
+    print(get_Armstrong(st,ed))
+    opt=input("Do You Want to Repeat y|n:")
+    if opt == 'n':
+        break
+
+
+
+
